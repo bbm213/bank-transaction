27d75e3ceb4a94889e741aec15dc41c1b93183fc
bbm213 <bbm.med.amine@gmail.com>
2021-09-22 15:38:27 +0100

    Transactions in a Month

--- bank-transaction/src/BankTransactionAnalyzer.java
+++ bank-transaction/src/BankTransactionAnalyzer.java
@@ -3,6 +3,9 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.time.LocalDate;
+import java.time.Month;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 
@@ -15,12 +18,26 @@ public class BankTransactionAnalyzer {
 		    final List<String> lines = Files.readAllLines(path);
 		    double total = 0d;
 		    for(final String line: lines) {
-		    final String[] columns = line.split(",");
-		    final double amount =
-		    Double.parseDouble(columns[1]);
-		    total += amount;
+			    final String[] columns = line.split(",");
+			    final double amount =
+			    Double.parseDouble(columns[1]);
+			    total += amount;
+		    }
+		    
+		    double totalDate = 0d;
+		    final DateTimeFormatter DATE_PATTERN =
+		    DateTimeFormatter.ofPattern("dd-MM-yyyy");
+			    for(final String line: lines) {
+			    final String[] columns = line.split(",");
+			    final LocalDate date = LocalDate.parse(columns[0],
+			    DATE_PATTERN);
+				    if(date.getMonth() == Month.JANUARY) {
+					    final double amountDate = Double.parseDouble(columns[1]);
+					    totalDate += amountDate;
+					 }	    
+			    }
+
+		    System.out.println("The total for all transactions is " + total);
+		    System.out.println("The total for all transactions in January is " + totalDate);
     }
-    
-    System.out.println("The total for all transactions is " + total);
-}
 }
